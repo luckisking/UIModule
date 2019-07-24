3f85245aec88fd793d5c32d39b24617336903b76
doxue_imac <>
2019-07-24 11:43:03 +0800

    移动文件夹

---
+++ UIModule/Develop/Public/Test.h
@@ -0,0 +1,17 @@
+//
+//  Test.h
+//  UIModule
+//
+//  Created by doxue_imac on 2019/7/24.
+//  Copyright © 2019 都学网. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Test : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
--- UIModule/ViewController.m
+++ UIModule/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "Test.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+   Test *view =  [[Test alloc] initWithFrame:self.view.bounds];
+    view.backgroundColor = [UIColor cyanColor];
+    self.view = view;
 }
 
 
